3a1ad164b2c9dbb8938dcfc9fac0cc9875a100e8
Ranika Williams <rnkprice07@gmail.com>
2019-08-18 17:49:56 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[6.0]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
